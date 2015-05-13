345688dffc9249c1517c5c46ce10e73621e6c770
oliviapriedeman <oapriedeman15@TBSLTMBA659.local>
2015-05-13 16:37:36 -0500

    some reactions to touches

--- TicTacTriYo/BoardViewController.m
+++ TicTacTriYo/BoardViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     //add the images
     oImg = [UIImage imageNamed:@"O.jpg"];
-    xImg = [UIImage imageNamed:@"X.jpg"];
+    xImg = [UIImage imageNamed:@"X.png"];
     //set the player to 1
     playerToken = 1;
     //update the label
@@ -38,11 +38,68 @@
     }
 }
 
+-(void)resetBoard{
+    self.s1.image = NULL;
+    self.s2.image = NULL;
+    self.s3.image = NULL;
+    self.s4.image = NULL;
+    self.s5.image = NULL;
+    self.s6.image = NULL;
+    self.s7.image = NULL;
+    self.s8.image = NULL;
+    self.s9.image = NULL;
+    
+    playerToken = 1;
+    self.whoseTurn.text = @"X goes first";
+    
 
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    UITouch *touch = [[event allTouches] anyObject];
+    if(CGRectContainsPoint([self.s1 frame], [touch locationInView:self.view])){
+        if(playerToken==1){self.s1.image = xImg;}
+        if(playerToken==2){self.s1.image = oImg;}
+    }
+    
+    if(CGRectContainsPoint([self.s2 frame], [touch locationInView:self.view])){
+        if(playerToken==1){self.s2.image = xImg;}
+        if(playerToken==2){self.s2.image =oImg;}
+    }
+    if(CGRectContainsPoint([self.s3 frame], [touch locationInView:self.view])){
+        if(playerToken==1){self.s3.image = xImg;}
+        if(playerToken==2){self.s3.image = oImg;}
+    }
+    if(CGRectContainsPoint([self.s4 frame], [touch locationInView:self.view])){
+        if(playerToken==1){self.s4.image = xImg;}
+        if(playerToken==2){self.s4.image = oImg;}
+    }
+    if(CGRectContainsPoint([self.s5 frame], [touch locationInView:self.view])){
+        if(playerToken==1){self.s5.image = xImg;}
+        if(playerToken==2){self.s5.image = oImg;}
+    }
+    if(CGRectContainsPoint([self.s6 frame], [touch locationInView:self.view])){
+        if(playerToken==1){self.s6.image = xImg;}
+        if(playerToken==2){self.s6.image = oImg;}
+    }
+    if(CGRectContainsPoint([self.s7 frame], [touch locationInView:self.view])){
+        if(playerToken==1){self.s7.image = xImg;}
+        if(playerToken==2){self.s7.image = oImg;}
+    }
+    if(CGRectContainsPoint([self.s8 frame], [touch locationInView:self.view])){
+        if(playerToken==1){self.s8.image = xImg;}
+        if(playerToken==2){self.s8.image = oImg;}
+    }
+    if(CGRectContainsPoint([self.s9 frame], [touch locationInView:self.view])){
+        if(playerToken==1){self.s9.image = xImg;}
+        if(playerToken==2){self.s9.image = oImg;}
+    }
 }
 
 /*
